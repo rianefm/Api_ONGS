cad7d8d660e2a4991d75b09ea4103a6b2652bafb
Riane <rianefmz@gmail.com>
2025-07-07 17:17:32 -0300

    � chore(api): enable CORS to allow requests from external frontends

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 import ongRoutes from './routes/ongsRoutes.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/', ongRoutes);
 
